c11d7c3e5a726da1ef147c1f5cfc4aa9a4237735
Iryna Bilyk <bilykirochka@gmail.com>
2024-01-29 11:40:00 +0200

    Update ContactItem.jsx

--- src/components/Contacts/ContactItem.jsx
+++ src/components/Contacts/ContactItem.jsx
@@ -2,7 +2,7 @@ import { Button } from './Contacts.styled';
 
 export const ContactItem = ({ id, name, number, onDelete }) => {
   return (
-    <li key={id}>
+    <li>
       <span>{name}: </span>
       <span>{number}</span>
       <Button type="button" onClick={() => onDelete(id)}>
